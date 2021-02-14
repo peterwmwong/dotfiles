c67e2df75f18e29d52032839eb76b5b36801df5b
Peter Wong <pwong@spins.com>
2021-02-14 14:58:12 -0600

    Handle coming back from suspension. Check if depot_tools, v8 and perf were already created.

--- install.sh
+++ install.sh
@@ -8,32 +8,43 @@ echo "    SETTING UP PWW CODESPACE    "
 echo "================================"
 echo ""
 
-echo "----------------------"
-echo "Setting up depot_tools"
-echo "----------------------"
-echo ""
-cd /home/vscode/workspace \
-    && git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git \
-    && export PATH=/home/vscode/workspace/depot_tools:$PATH
+echo "Checking for depot_tools..."
+if [ ! -d "/home/vscode/workspace/depot_tools" ]; then
+    echo "----------------------"
+    echo "Setting up depot_tools"
+    echo "----------------------"
+    echo ""
+    cd /home/vscode/workspace
+    git clone https://chromium.googlesource.com/chromium/tools/depot_tools.git
+    export PATH=/home/vscode/workspace/depot_tools:$PATH
+    echo "\nexport PATH=/home/vscode/workspace/depot_tools:\$PATH\n" >> ~/.zshrc
+fi
 
-echo "-------------------------"
-echo "Installing v8 depedencies"
-echo "-------------------------"
-echo ""
-cd /home/vscode/workspace \
-    && mkdir v8 && cd v8 \
-    && fetch v8 \
-    && cd v8 \
-    && build/install-build-deps.sh
+echo "Checking for v8 and dependencies..."
+if [ ! -d "/home/vscode/workspace/v8" ]; then
+    echo "---------------------------------------"
+    echo "Fetching v8 and installing dependencies"
+    echo "---------------------------------------"
+    echo ""
+    cd /home/vscode/workspace
+    mkdir v8 
+    cd v8
+    fetch v8
+    cd v8
+    build/install-build-deps.sh
+fi
 
-echo "----------------------------------"
-echo "Building & installing perf command"
-echo "----------------------------------"
-echo ""
-cd /home/vscode/workspace \
-    && mkdir kernel && cd kernel \
-    && git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git \
-    && cd tip/tools/perf \
-    && make \
-    && sudo ln -s /home/vscode/workspace/kernel/tip/tools/perf/perf /usr/bin/perf
-
+echo "Checking for perf..."
+if [ ! -d "/home/vscode/workspace/kernel/tip/tools/perf" ]; then
+    echo "----------------------------------"
+    echo "Building & installing perf command"
+    echo "----------------------------------"
+    echo ""
+    cd /home/vscode/workspace
+    mkdir kernel
+    cd kernel
+    git clone --depth 1 git://git.kernel.org/pub/scm/linux/kernel/git/tip/tip.git
+    cd tip/tools/perf
+    make
+    echo "\nexport PATH=/home/vscode/workspace/kernel/tip/tools/perf/perf:\$PATH\n" >> ~/.zshrc
+fi
